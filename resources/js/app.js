e1c3bbcc2ca7acf1dd92fde9edc80057b4310f8c
nerdroid23 <nerdroid23@gmail.com>
2021-08-03 23:56:58 -0400

    wip - vite & inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-require('./bootstrap');
+import 'vite/dynamic-import-polyfill'
+import '../css/app.css'
+
+import './bootstrap'
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+
+
+InertiaProgress.init()
+
+createInertiaApp({
+  resolve: async (name) => {
+    return (await import(`./Pages/${ name }.vue`)).default
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('', 'welcome');
+Route::get('', function () {
+    return inertia('Welcome');
+});
